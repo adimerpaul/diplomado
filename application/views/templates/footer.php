75740bcd9b7fbfd35313f49dd6a0b20913ed0b82
adimerpaul <adimer101@gmail.com>
2024-06-21 23:10:30 -0400

    actulizado para  mostrar 100

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -62,6 +62,7 @@
             });
         });
         $('#example').DataTable({
+            pageLength: 100,
             "language": {
                 "url": "https://cdn.datatables.net/plug-ins/2.0.3/i18n/es-ES.json"
             }
